8cb0cca92c1c193964e6490bf9e821956608e6a8
Jeongin <89836139+jxxngin@users.noreply.github.com>
2025-04-18 16:32:33 +0900

    [level 0] Title: 약수 구하기, Time: 0.16 ms, Memory: 86.9 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -4,11 +4,17 @@ class Solution {
     public int[] solution(int n) {
         ArrayList<Integer> nums = new ArrayList<>();
         
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i <= Math.sqrt(n); i++) {
             if (n % i == 0) {
                 nums.add(i);
+                
+                if (i != n / i) {
+                    nums.add(n / i);
+                }
             } 
         }
+        
+        Collections.sort(nums);
         
         int len = nums.size();
         int[] answer = new int[len];
